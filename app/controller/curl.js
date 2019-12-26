90808e296270e7642b17f6ed7007b26785398171
lsj <1109833087@qq.com>
2019-12-26 23:14:17 +0800

    尝试文件流

--- app/controller/curl.js
+++ app/controller/curl.js
@@ -7,12 +7,16 @@ const createRule = {
 class curlController extends Controller {
     async index() {
         const ctx = this.ctx;
-        const content = ctx.request.body
+        var content = ctx.request.body
         var result = {}
+        var formdata = await this.ctx.getFileStream()
+        this.ctx.logger.info(formdata);
         if (content.hasOwnProperty('method')&&content.method === 'post'){
+            this.ctx.logger.info(content);
             result = await ctx.service.mycurl.post(content);
         }else {
-            result = await ctx.service.mycurl.get(content);
+            this.ctx.logger.info(content,'curl');
+            result = await ctx.service.mycurl.post(content);
         }
         // 设置响应体和状态码
         ctx.body = {
--- app/service/mycurl.js
+++ app/service/mycurl.js
@@ -2,7 +2,13 @@ const Service = require('egg').Service;
 
 class NewsService extends Service {
     async post(params) {
-        const result = await this.ctx.curl(params.url, {
+        this.ctx.logger.info(params,'service');
+        if (params.hasOwnProperty('headers')&&params.headers.hasOwnProperty('Content-Type')&&params.headers['Content-Type']==='multipart/form-data')
+        {
+            this.ctx.logger.info('222')
+            this.ctx.logger.info(params.data.name)
+        }
+       /* const result = await this.ctx.curl(params.url, {
             // 必须指定 method
             method: 'POST',
             // 通过 contentType 告诉 HttpClient 以 JSON 格式发送
@@ -10,8 +16,10 @@ class NewsService extends Service {
             headers:params.hasOwnProperty('headers')?params.headers:{contentType: 'json'},
             // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
             dataType: 'json',
-        });
-        return result.data
+        }).catch(e=>{
+            this.ctx.logger.info(e);
+        });*/
+        return "result.data"
     }
     async get(params) {
         const result = await this.ctx.curl(params.url, {
