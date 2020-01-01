05a362b2200c0de4177618280910f76ed421f2ba
lsj <1109833087@qq.com>
2020-01-01 23:12:17 +0800

    more

--- app/router.js
+++ app/router.js
@@ -1,8 +1,8 @@
 module.exports = app => {
     const { router, controller } = app;
-    router.get('/', controller.home.index);
-    router.post('/api/curl', controller.procurl.index);
-    router.post('/api/more', controller.more.index);
-    router.get('/api/more', controller.more.index);
-    router.post('/api/speech', controller.speech.index);
+    router.get('/', controller.home.index);// helloworld
+    router.post('/api/curl', controller.procurl.index); // curl接口，跨域的接口
+    router.post('/api/more', controller.more.index);// 部署的接口
+    router.get('/api/more', controller.more.index);//  getmore的接口，暂时没用
+    router.post('/api/speech', controller.speech.index); // 语音识别的接口
 };
