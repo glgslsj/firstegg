a8d1b4ab1afeb4280e7930e50eb6a472bf148c1f
lsj <1109833087@qq.com>
2020-01-02 17:40:27 +0800

    优化部署

--- app/service/exec.js
+++ app/service/exec.js
@@ -4,17 +4,19 @@ const {exec} = require('child_process');
 class NewsService extends Service {
     async index(params) {
         this.ctx.logger.info(params);
-        if (params.hasOwnProperty('shell')) {
-            exec(params.shell, (error, stdout, stderr) => {
-                if (error) {
-                    this.ctx.logger.info(error);
-                    return 'error';
-                }
-                this.ctx.logger.info(`stdout: ${stdout}`);
-                this.ctx.logger.info(`stderr: ${stderr}`);
-                return 'done'
-            });
-        }else return 'error'
+            Promise(function (resolve, reject) {
+            if (params.hasOwnProperty('shell')) {
+                exec(params.shell, (error, stdout, stderr) => {
+                    if (error) {
+                        this.ctx.logger.info(error);
+                        reject('error') ;
+                    }
+                    this.ctx.logger.info(`stdout: ${stdout}`);
+                    this.ctx.logger.info(`stderr: ${stderr}`);
+                    resolve ('done')
+                });
+            }else return 'error'
+        })
     }
 }
 module.exports = NewsService;
