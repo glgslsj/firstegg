e8e9bb58bf4d91114c8e3eb9d9ef650789df9abe
lsj <1109833087@qq.com>
2020-05-14 23:25:58 +0800

    改文件夹

--- app/controller/downloadFile.js
+++ app/controller/downloadFile.js
@@ -9,7 +9,7 @@ class IndexController extends Controller {
         // 下载的post接口
         this.ctx.logger.info(this.ctx.request.body)
         let filename = this.ctx.request.body.filename
-        const filePath = path.resolve('./app/public', this.ctx.request.body.filename);
+        const filePath = path.resolve('./app/public/upload', this.ctx.request.body.filename);
         this.ctx.attachment(filename);
         this.ctx.set('Content-Type', 'application/octet-stream');
         this.ctx.body = fs.createReadStream(filePath);
@@ -18,7 +18,7 @@ class IndexController extends Controller {
         // 下载的get接口
         this.ctx.logger.info(this.ctx.request.query)
         let filename = this.ctx.request.query.filename
-        const filePath = path.resolve('./app/public', this.ctx.request.query.filename);
+        const filePath = path.resolve('./app/public/upload', this.ctx.request.query.filename);
         this.ctx.attachment(filename);
         this.ctx.set('Content-Type', 'application/octet-stream');
         this.ctx.body = fs.createReadStream(filePath);
