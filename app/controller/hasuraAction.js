540edf6ec3a8038cc73ded7106c5673795a8fb72
lsj <1109833087@qq.com>
2020-08-17 11:41:08 +0800

    jwt

--- app/controller/hasuraAction.js
+++ app/controller/hasuraAction.js
@@ -1,36 +1,37 @@
 const Controller = require('egg').Controller;
+import * as jwt from 'jsonwebtoken';
 
 class HasuraActionController extends Controller {
+    verityJWT(JWT) {
+        let jwtkey = '8YPncVonxi0afQGDbPfIfTpna5nFFIZ4';
+        return jwt.verify(JWT, this.jwtkey, (err, payload) => {
+            // if token alg != RS256,  err == invalid signature
+            if (err) {
+                throw { desc: 'token errors', content: err, errCode: 102 };
+            }
+            return payload;
+        });
+    }
     async index() {
+        var jwtkey = '8YPncVonxi0afQGDbPfIfTpna5nFFIZ4';
         const { app, ctx } = this;
         const { username } = ctx.request.body.input.account;
-        let content = {token:'username'}
+        /*let content = {token:'username'}
         if (username){
             content.token = username
-        }
+        }*/
 
-        /*const mailOptions = {
-            from: '1109833087@qq.com',
-            to: '359718689@qq.com',
-            subject: 'hello world',
-            html: `<div>点击链接进行验证</div>`
-        };
-
-        app.email.sendMail(mailOptions, (error, response) => {
-            if (error) {
-                ctx.logger.info(error);
-                content = error
-                ctx.body = content;
-                ctx.status = 201;
-            } else {
-                ctx.logger.info(response.message);
-                content = response.message
-                ctx.body = content;
-                ctx.status = 201;
-            }
-            app.email.close();
-        });*/
-        ctx.body = content;
+        const token = jwt.sign(
+            {
+                'https://hasura.io/jwt/claims': {
+                    'x-hasura-allowed-roles': [ 'admin','guest' ],
+                    'x-hasura-default-role': 'admin',
+                },
+            },
+            jwtkey,
+            // 第二个字段设置有效期为30天内
+            { algorithm: 'HS256', expiresIn: '30 days' });
+        ctx.body = token;
         ctx.status = 201;
     }
     async login() {
