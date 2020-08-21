d29a524b4ebb3a03b9606f2a154c6ac65389822b
lsj <1109833087@qq.com>
2020-08-21 12:29:11 +0800

    newapi

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     const { router, controller } = app;
     router.get('/', controller.home.index);// helloworld，测试接口
+    router.get('/test', controller.home.test);// helloworld，测试接口
     router.post('/api/curl', controller.procurl.index); // curl接口，跨域的接口
     router.post('/api/more', controller.more.index);// 部署的接口
     router.get('/api/more', controller.more.index);//  getmore的接口，暂时没用
